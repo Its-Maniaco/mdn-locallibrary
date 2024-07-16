26175c9a0d2df39dd46751c7eaf5f9a8874510ff
Its-Maniaco <21123621+Its-Maniaco@users.noreply.github.com>
2024-07-16 21:47:50 +0530

    added delete genre
    Signed-off-by: Its-Maniaco <21123621+Its-Maniaco@users.noreply.github.com>

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -137,6 +137,8 @@ exports.author_delete_post = asyncHandler(async (req, res, next) => {
     return;
   } else {
     // Author has no books. Delete object and redirect to the list of authors.
+    /* `authorid` is expected to be included in the request body when the form is submitted. 
+    Thus, the form used for deleting should have an input field with the name `authorid`*/
     await Author.findByIdAndDelete(req.body.authorid);
     res.redirect("/catalog/authors");
   }
